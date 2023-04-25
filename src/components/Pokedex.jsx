506c9c9e0fcb897aeefa31fbbd4bd70b6dbc625a
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-04-25 19:27:06 +0000

    Working to fix styling and set up rows without overflow

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -16,24 +16,56 @@ function Pokedex () {
   };
   const pokemon = pokemonData.map((data) => {
     return (
-    <div>
-      <img src={getImageUrl(data.id)}></img>
-      <p>Name: {data.name.english}</p>
-      <ul>
-        <li>Type: {data.type[0]} / {data.type[1]}</li>
-        <li>Base Stats:</li>
-        <li>HP: {data.base.HP}</li> 
-        <li>Attack: {data.base.Attack}</li> 
-        <li>Defense: {data.base.Defense}</li>
-        <li>Sp. Attack: {data.base["Sp. Attack"]}</li>
-        <li>Sp. Defense: {data.base["Sp. Defense"]}</li>
-        <li>Speed: {data.base.Speed}</li>
+    <div key={data.id} className="col p-3">
+      <img src={getImageUrl(data.id)} className="img-flex"></img>
+      <p><strong>{data.name.english}</strong></p>
+      <ul className="list-group">
+        <li className="list-group-item">{data.type[0]} / {data.type[1]}</li>
+        <li className="list-group-item">HP: {data.base.HP}</li> 
+        <li className="list-group-item">Attack: {data.base.Attack}</li> 
+        <li className="list-group-item">Defense: {data.base.Defense}</li>
+        <li className="list-group-item">Sp. Attack: {data.base["Sp. Attack"]}</li>
+        <li className="list-group-item">Sp. Defense: {data.base["Sp. Defense"]}</li>
+        <li className="list-group-item">Speed: {data.base.Speed}</li>
       </ul>
     </div>
       )
   })
+  /*
+  const rows = pokemon.map((col) => {
+    let row = []
+    for (let i = 0; i < pokemon.length; i++){
+      let minirow = []
+      minirow.push(col)
+    }
+  })
+  */
+
+  /*
+
+  function rowGen(arr){
+    const size = 5; 
+    let arrayOfArrays = [];
+    for (let i=0; i < arr.length; i += size) {
+     arrayOfArrays.push(arr.slice(i, i+size));
+    }
+    console.log(arrayOfArrays[0])
+  }
+  const rowsArr = rowGen(pokemon)
+  const rows = rowsArr.map((row) => {
+    return (
+      <div className="row">
+        {row}
+      </div>
+    )
+  })
+  */
   return (
-    pokemon
+    <div className="container">
+      <div className="row p-4">
+        {pokemon}
+      </div>
+    </div>
   )
 }
 
